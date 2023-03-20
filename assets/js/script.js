c03282c24dba39d6aa9433eac036fc06685e3bc6
pphilippersson6 <philip.persson.code@gmail.com>
2023-03-20 10:58:27 +0000

    added the game and possebilities for the game. rock paper scissor

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,24 @@ function welcomeAlert () {
           "Hello " + person + "! It's time to play!";
       }
 }
+
+
+
+// game //
+
+function checkWinner(compChoice, playerChoice) {
+
+    let result;
+    if (compChoice === playerChoice) {
+      result = "draw";
+    } else if (compChoice == "Rock" && playerChoice == "Scissors" ||
+      compChoice == "Scissors" && playerChoice == "Paper" ||
+      compChoice == "Paper" && playerChoice == "Rock") {
+  
+      result = "computer"
+    } else {
+      result = "player";
+    }
+  
+    return result;
+  }
