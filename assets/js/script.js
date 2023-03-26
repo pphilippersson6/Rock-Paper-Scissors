9b6768874723e42f5035ad1508efa20a901b2235
pphilippersson6 <philip.persson.code@gmail.com>
2023-03-26 12:04:26 +0000

    fixed some JS style and made it work better. fixed small errors.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,37 @@
 const choices = ["Rock", "Paper", "Scissors"];
 const message = document.getElementById("statistics");
-let totalPlayed = 0, rock = 0, paper = 0, scissors = 0;
+let totalPlayed = 0;
+let rock = 0;
+let paper = 0;
+let scissors = 0;
+
 
 
 // welcome message/alert  //
-function welcomeAlert () {
-    let person = prompt ("Please enter your name:");
-    if (person != null) {
-        document.getElementById("headingName").innerHTML =
-          "Please " + person + ", choose wisely!";
-      }
+function welcomeAlert() {
+  let person = prompt("Please enter your name:");
+  if (person != null) {
+    document.getElementById("headingName").innerHTML =
+      "Please " + person + ", choose wisely!";
+  }
 }
 
+let submit = document.getElementById("btnSubmit")
+submit.addEventListener("click", submitAnswer)
 
 // game //
-
 function submitAnswer() {
-
-
   const choice = document.querySelector("input[name='choice']:checked");
-
+  console.log(choice)
   if (choice) {
     let playerChoice = choice.value;
-    let computerChoice = Math.floor(Math.random() * choices.length);
+    let computerNumber = Math.floor(Math.random() * choices.length);
+    let computerChoice = choices[computerNumber]
 
     document.getElementById("player-choice").textContent = playerChoice;
 
     document.querySelectorAll(".computer-choice").forEach(element => {
-      element.textContent = choices[computerChoice];
+      element.textContent = computerChoice;
     });
 
     switch (playerChoice) {
@@ -44,65 +48,65 @@ function submitAnswer() {
         break;
     }
 
-    let result = checkWinner(choices[computerChoice], playerChoice);
-
-    updateScores(result);
-
+    let winner = checkWinner(computerChoice, playerChoice); 
+    updateScores(winner)
   }
-
 }
 
 // game-result //
 
 function checkWinner(compChoice, playerChoice) {
+  let result;
+  if (compChoice === playerChoice) {
+    result = "draw";
+  } 
+  else if (compChoice == "Rock" && playerChoice == "Scissors" ||
+    compChoice == "Scissors" && playerChoice == "Paper" ||
+    compChoice == "Paper" && playerChoice == "Rock") {
 
-  
-
-    let result;
-    if (compChoice === playerChoice) {
-      result = "draw";
-    } else if (compChoice == "Rock" && playerChoice == "Scissors" ||
-      compChoice == "Scissors" && playerChoice == "Paper" ||
-      compChoice == "Paper" && playerChoice == "Rock") {
-  
-      result = "computer"
-    } else {
-      result = "player";
-    }
-  
-    return result;
+    result = "computer"
+  } else {
+    result = "player";
   }
 
-  function updateScores(result) {
-    const playerScore = document.getElementById("player-score");
-    const computerScore = document.getElementById("computer-score");
-    const total = document.getElementById("total");
-    const rockUsed = document.getElementById("rock-used");
-    const paperUsed = document.getElementById("paper-used");
-    const scissorsUsed = document.getElementById("scissors-used");
+  return result;
+}
+
+function updateScores(result) {
+  const playerScore = document.getElementById("player-score");
+  const computerScore = document.getElementById("computer-score");
+  const total = document.getElementById("total");
+  console.log(result)
   
-    let newScore;
-    totalPlayed++;
-  
-    total.textContent = totalPlayed;
-    rockUsed.textContent = rock;
-    paperUsed.textContent = paper;
-    scissorsUsed.textContent = scissors;
-  
-    if (result == "draw") {
-      message.textContent = "It is a draw!";
-    }
-  
-    if (result === "computer") {
-      newScore = parseInt(computerScore.textContent) + 1;
-      computerScore.textContent = newScore;
-      message.textContent = "Computer won!";
-    }
-  
-    if (result === "player") {
-      newScore = parseInt(playerScore.textContent) + 1;
-      playerScore.textContent = newScore;
-      message.textContent = "You won!";
-    }
-  
+  // const rockUsed = document.getElementById("rock-used");
+  // const paperUsed = document.getElementById("paper-used");
+  // const scissorsUsed = document.getElementById("scissors-used");
+
+  let newScore;
+  totalPlayed++;
+
+  total.textContent = totalPlayed;
+  // rockUsed.textContent = rock;
+  // paperUsed.textContent = paper;
+  // scissorsUsed.textContent = scissors;
+
+  if (result === "draw") {
+    message.textContent = "It is a draw!";
+    console.log("draw")
   }
+
+  if (result === "computer") {
+    newScore = parseInt(computerScore.textContent) + 1;
+    computerScore.textContent = newScore;
+    message.textContent = "Computer won!";
+    console.log("computer won")
+  }
+
+  if (result === "player") {
+    newScore = parseInt(playerScore.textContent) + 1;
+    playerScore.textContent = newScore;
+    message.textContent = "You won!";
+    console.log("player")
+  }
+
+}
